c37cfcd104c6285012e7b92d1d2b5a5e61a2febd
Raghavan <raghavanvenkatesan018@gmail.com>
2022-10-15 19:00:18 +0530

    Adding error Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const products = require('./data/products');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes');
+const {notFound, errorHandler} = require('./middleware/errorMiddleware'); 
 
 // import express from 'express';
 // import dotenv from 'dotenv';
 // import products from './data/products.js';
+
+// normal middleware
+// app.use((req, res, next) => {
+//     console.log(req.originalUrl);
+// })
 
 dotenv.config();
 
@@ -13,14 +19,36 @@ connectDB();
 
 const app = express();
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-})
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id);
-    res.json(product);
-})
+//passing the 404 error to the error middleware
+// app.use((req, res, next) => {
+//     const error = new Error(`Not Found - ${req.originalUrl}`);
+//     res.status(404);
+//     next(error);
+// });
+
+app.use(notFound);
+
+//error middleware
+// app.use((err, req, res, next) => {
+//     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+//     res.status(statusCode);
+//     res.json({
+//         message: err.message,
+//         stack: process.env.NODE_ENV === 'production' ? null : err.stack
+//     })
+// })
+
+app.use(errorHandler);
+// app.get('/api/products', (req, res) => {
+//     res.json(products);
+// })
+
+// app.get('/api/products/:id', (req, res) => {
+//     const product = products.find(p => p._id === req.params.id);
+//     res.json(product);
+// })
 
 const PORT = process.env.PORT || 5000
 
