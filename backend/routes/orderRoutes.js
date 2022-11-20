bd9ab1c920871c133fad7b85031b6af585509866
Raghavan <raghavanvenkatesan018@gmail.com>
2022-11-20 08:46:16 +0530

    Added the functionality of Order route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,9 +2,10 @@ const protect = require('../middleware/authMiddleware');
 
 const express = require('express');
 const router  = express.Router();
-const { addOrderItems, getOrderById } = require('../controllers/orderContoller');
+const { addOrderItems, getOrderById, updateOrderToPaid } = require('../controllers/orderContoller');
 
 router.route('/').post(protect, addOrderItems);
-router.route('/:id').get(protect, getOrderById)
+router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 module.exports = router;
